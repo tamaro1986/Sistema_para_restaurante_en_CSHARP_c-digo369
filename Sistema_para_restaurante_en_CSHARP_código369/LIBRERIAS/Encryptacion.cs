a2c051e8ab7c70e309a18bd6f25111d07f570b38
Enrique Garcia <mateogaray780@gmail.com>
2021-06-23 14:41:43 -0600

    Agregar archivos de proyecto.

---
+++ Sistema_para_restaurante_en_CSHARP_código369/LIBRERIAS/Encryptacion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Sistema_para_restaurante_en_CSHARP_código369.LIBRERIAS
+{
+	class Encryptacion
+	{
+		private byte[] Encrypt(byte[] clearData, byte[] Key, byte[] IV)
+		{
+			MemoryStream ms = new MemoryStream();
+			Rijndael alg = Rijndael.Create();
+			alg.Key = Key;
+			alg.IV = IV;
+			CryptoStream cs = new CryptoStream(ms, alg.CreateEncryptor(), CryptoStreamMode.Write);
+			cs.Write(clearData, 0, clearData.Length);
+			cs.Close();
+			byte[] encryptedData = ms.ToArray();
+			return encryptedData;
+
+		}
+
+		
+
+	}
+}
