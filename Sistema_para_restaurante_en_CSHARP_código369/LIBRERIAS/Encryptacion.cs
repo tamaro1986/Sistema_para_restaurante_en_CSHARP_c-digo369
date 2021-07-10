f1405dc15f71391c03324dd98c54b91b8df0535f
Enrique Garcia <mateogaray780@gmail.com>
2021-07-10 11:44:21 -0600

    Agregar archivos de proyecto.

--- Sistema_para_restaurante_en_CSHARP_código369/LIBRERIAS/Encryptacion.cs
+++ Sistema_para_restaurante_en_CSHARP_código369/LIBRERIAS/Encryptacion.cs
@@ -20,8 +20,84 @@ namespace Sistema_para_restaurante_en_CSHARP_código369.LIBRERIAS
 			cs.Close();
 			byte[] encryptedData = ms.ToArray();
 			return encryptedData;
-
 		}
+
+		public string Encrypt(string Data, string Password, int Bits)
+		{
+			byte[] clearBytes = System.Text.Encoding.Unicode.GetBytes(Data);
+			PasswordDeriveBytes pdb = new PasswordDeriveBytes(Password, new byte[] { 0x0, 0x1, 0x2, 0x1C, 0x1D, 0x1E, 0x3, 0x4, 0x5, 0xF, 0x20, 0x21, 0xAD, 0xAF, 0xA4 });
+			if (Bits == 128)
+			{
+				byte[] encryptedData = Encrypt(clearBytes, pdb.GetBytes(16), pdb.GetBytes(16));
+				return Convert.ToBase64String(encryptedData);
+			}
+			else if (Bits == 192)
+			{
+				byte[] encryptedData = Encrypt(clearBytes, pdb.GetBytes(24), pdb.GetBytes(16));
+				return Convert.ToBase64String(encryptedData);
+			}
+			else if (Bits == 256)
+			{
+				byte[] encryptedData = Encrypt(clearBytes, pdb.GetBytes(32), pdb.GetBytes(16));
+				return Convert.ToBase64String(encryptedData);
+			}
+			else
+			{
+				return String.Concat(Bits);
+			}
+		}
+
+		public byte[] Decrypt(byte[] cipherData, byte[] Key, byte[] IV)
+		{
+			MemoryStream ms = new MemoryStream();
+			Rijndael alg = Rijndael.Create();
+			alg.Key = Key;
+			alg.IV = IV;
+			CryptoStream cs = new CryptoStream(ms, alg.CreateDecryptor(), CryptoStreamMode.Write);
+			cs.Write(cipherData, 0, cipherData.Length);
+			cs.Close();
+			byte[] decryptedData = ms.ToArray();
+			return decryptedData;		
+		}
+
+		public string Decrypt(string Data, string Password, int Bits)
+		{
+			try
+			{
+				byte[] cipherBytes = Convert.FromBase64String(Data);
+				PasswordDeriveBytes pdb = new PasswordDeriveBytes(Password, new byte[] { 0x0, 0x1, 0x2, 0x1C, 0x1D, 0x1E, 0x3, 0x4, 0x5, 0xF, 0x20, 0x21, 0xAD, 0xAF, 0xA4 });
+				
+				if (Bits == 128)
+				{
+					byte[] decryptedData = Decrypt(cipherBytes, pdb.GetBytes(16), pdb.GetBytes(16));
+					return System.Text.Encoding.Unicode.GetString(decryptedData);
+				}
+				else if (Bits == 192)
+				{
+					byte[] decryptedData = Decrypt(cipherBytes, pdb.GetBytes(24), pdb.GetBytes(16));
+					return System.Text.Encoding.Unicode.GetString(decryptedData);
+				}
+				else if (Bits == 256)
+				{
+					byte[] decryptedData = Decrypt(cipherBytes, pdb.GetBytes(32), pdb.GetBytes(16));
+					return System.Text.Encoding.Unicode.GetString(decryptedData);
+				}
+				else
+				{
+					return String.Concat(Bits);					
+				}
+			}
+			catch (Exception ex)
+			{
+				return String.Concat(Bits);
+			
+			}
+		
+		
+		}
+
+
+
 
 		
 
